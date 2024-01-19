1a1ddeba736887e31e43c31bb74a21bfa1c59a32
Emma Lorensson <emma.lorensson@outlook.com>
2024-01-19 16:30:52 +0100

    Delete dist directory

--- dist/index.231adef9.js
+++
@@ -1,2 +0,0 @@
-window.onload=function(){let e=document.querySelector(".hamburger"),t=document.querySelector(".mobile_nav");e.addEventListener("click",function(){e.classList.toggle("active"),t.classList.toggle("active")})};
-//# sourceMappingURL=index.231adef9.js.map
