d18fb6b3397001773755e14bc5ddf2196a038f07
Emma Lorensson <emma.lorensson@outlook.com>
2024-01-19 14:59:41 +0100

    commit för att terminalen säger det

---
+++ dist/index.0b126077.js
@@ -0,0 +1,2 @@
+window.onload=function(){let e=document.querySelector(".hamburger"),t=document.querySelector(".mobile_nav");e.addEventListener("click",function(){e.classList.toggle("active"),t.classList.toggle("active")})};
+//# sourceMappingURL=index.0b126077.js.map
---
+++ dist/index.231adef9.js
@@ -0,0 +1,2 @@
+window.onload=function(){let e=document.querySelector(".hamburger"),t=document.querySelector(".mobile_nav");e.addEventListener("click",function(){e.classList.toggle("active"),t.classList.toggle("active")})};
+//# sourceMappingURL=index.231adef9.js.map
